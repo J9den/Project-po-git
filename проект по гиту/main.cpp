404b19343a6c67ee403ef058e852ff3020be3f7a
Darina Gneusneva <krokodilka2006@gmail.com>
2024-09-24 11:27:18 +0300

    Initial Commit

---
+++ проект по гиту/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  проект по гиту
+//
+//  Created by Дарина on 24.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
